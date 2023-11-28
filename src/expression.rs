f6bffdfc5841752958688284983764a5cf4110d0
theMackabu <theMackabu@gmail.com>
2023-11-28 00:31:20 -0800

    migrate macros-rs out of justjs

---
+++ src/expression.rs
@@ -0,0 +1,50 @@
+#[macro_export]
+macro_rules! inc {
+    ($id:expr) => {{
+        let _rv = $id;
+        $id += 1;
+        _rv
+    }};
+}
+
+#[macro_export]
+macro_rules! dec {
+    ($id:expr) => {{
+        let _rv = $id;
+        $id -= 1;
+        _rv
+    }};
+}
+
+#[macro_export]
+macro_rules! ternary {
+    ($c:expr, $v:expr, $v1:expr) => {
+        if $c {
+            $v
+        } else {
+            $v1
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! then {
+    ($c:expr, $v:expr) => {
+        if $c {
+            $v
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! attempt {
+   (@recurse ($a:expr) { } catch ($e:ident) $b:block) => {
+      if let Err ($e) = $a $b
+   };
+   (@recurse ($a:expr) { $e:expr; $($tail:tt)* } $($handler:tt)*) => {
+      attempt!{@recurse ($a.and_then (|_| $e)) { $($tail)* } $($handler)*}
+   };
+   ({ $e:expr; $($tail:tt)* } $($handler:tt)*) => {
+      attempt!{@recurse ($e) { $($tail)* } $($handler)* }
+   };
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod expression;
+pub mod format;
+pub mod function;
+pub mod interface;
